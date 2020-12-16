7d12294cc78c7836e3435149a13ab15638591471
Sofia2506 <sonque@unsa.edu.pe>
2020-12-16 15:43:10 -0500

    no funciona el reconocimiento :c

---
+++ index.js
@@ -0,0 +1,45 @@
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const texto = document.getElementById("texto");
+const playText = document.getElementById("playText");
+
+let recognition = new webkitSpeechRecognition();
+recognition.lang = "es-Es";
+recognition.continuous = true;
+recognition.interimResults = false;
+
+recognition.onresult = (event) => {
+    const results = event.results;
+    const frase = results[results.length - 1][0].transcript;
+    texto.value += frase;
+}
+
+recognition.onend = (event) => {
+    console.log("El micrófono ha dejado de escuchar");
+}
+
+recognition.onerror = (event) => {
+    console.log(event.error);
+}
+
+startButton.addEventListener("click", () => {
+    recognition.start();
+});
+
+stopButton.addEventListener("click", () => {
+    recognition.abort();
+});
+
+playText.addEventListener("click", () => {
+    leerTexto(texto.value);
+});
+
+function leerTexto(texto){
+    const speech = new SpeechSynthesisUtterance();
+    speech.text = texto;
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+
+    window.speechSynthesis.speak(speech);
+}
