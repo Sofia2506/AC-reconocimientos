361ef095c60adae7739473ef36e98296f26bc8ec
Sofia2506 <sonque@unsa.edu.pe>
2020-12-31 03:40:27 -0500

    se puede editar y con la x 'guardas'

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const stopButton = document.getElementById("stop");
 const texto = document.getElementById("texto");
 const playText = document.getElementById("playText");
 const limpiarButton = document.getElementById("limpiar");
+const GuardarButton = document.getElementById("guardar");
+var Nota = document.getElementById("texto");
+var guardar = document.getElementById("x");
+
 
 let recognition = new webkitSpeechRecognition();
 recognition.lang = "es-Es";
@@ -39,6 +43,25 @@ limpiarButton.addEventListener("click", () => {
     texto.value = null;
 });
 
+GuardarButton.addEventListener("click", () => {
+
+    //parte de la nota en sí
+    console.log(Nota.value);
+    document.getElementById("nota").innerHTML = Nota.value;
+    //parte de fecha y hora
+	var current_date = new Date();
+
+	var fecha = `${current_date.getDate()}/${current_date.getMonth()+1}/${current_date.getFullYear()}`
+    var hora = `${current_date.getHours()}:${current_date.getMinutes()}`
+    document.getElementById("fecha").innerHTML = fecha;
+    document.getElementById("hora").innerHTML = hora;
+});
+
+guardar.addEventListener("click", () => {
+    //se debería guardar y ... por ende se borra automaticamente la nota :v
+    texto.value = null;
+});
+
 function leerTexto(texto){
     const speech = new SpeechSynthesisUtterance();
     speech.text = texto;
@@ -48,3 +71,5 @@ function leerTexto(texto){
 
     window.speechSynthesis.speak(speech);
 }
+
+
